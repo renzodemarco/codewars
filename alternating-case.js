b4936edd29737d49e77e89bfe1b36055e7824f4e
Renzo Demarco <renzo11demarco@gmail.com>
2023-03-02 14:23:43 -0300

    new codewars challenge

---
+++ alternating-case.js
@@ -0,0 +1,13 @@
+// Define String.prototype.toAlternatingCase such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. 
+
+// My Solution:
+
+String.prototype.toAlternatingCase = function () {
+    let result = "";
+    for (i = 0; i < this.length; i++) {
+        if (this[i] == this[i].toUpperCase()) {
+            result += this[i].toLowerCase();
+        } else result += this[i].toUpperCase();
+    }
+    return result;
+}
