072a4ca2d22f2f7f7b4ff0b4b02f7c938260d6e9
Renzo Demarco <renzo11demarco@gmail.com>
2023-03-06 18:54:35 -0300

    new codewars challenge

---
+++ switch-it-up.js
@@ -0,0 +1,30 @@
+// When provided with a number between 0-9, return it in words.
+
+// My Solution:
+
+function switchItUp(number) {
+    switch (number) {
+        case 0:
+            return "zero";
+        case 1:
+            return "one";
+        case 2:
+            return "two";
+        case 3:
+            return "three";
+        case 4:
+            return "four";
+        case 5:
+            return "five";
+        case 6:
+            return "six";
+        case 7:
+            return "seven";
+        case 8:
+            return "eight";
+        case 9:
+            return "nine";
+        default: "only numbers between 0 and 9 are valid"
+            break;
+    }
+}
