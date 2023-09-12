d6975e2ee05094c1084de4dd3a0eaad657cdcf6f
Renzo Demarco <renzo11demarco@gmail.com>
2023-09-12 15:11:29 -0300

    new codewars challenge

--- consecutive-strings.js
+++ consecutive-strings.js
@@ -1,4 +1,16 @@
 // You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
 
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
 // My Solution: 
 
+function longestConsec(strarr, k) {
+    let longest = ''
+    for (i = 0; k > 0 && i <= strarr.length - k; i++) {
+        const word = strarr.slice(i, i + k).join('')
+        if (word.length > longest.length) {
+            longest = word
+        }
+    }
+    return longest
+}
