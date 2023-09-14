07f5eab3f63e7ca2f0cc306c052d271c470be8b1
Renzo Demarco <renzo11demarco@gmail.com>
2023-09-14 11:39:26 -0300

    new codewars challenge

---
+++ algo.js
@@ -0,0 +1,10 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+// My Solution:
+
+function findShort(s) {
+    const ordered = s.split(' ').sort((a, b) => a.length - b.length)
+    return ordered[0].length
+}
