f353ee1747610ee5cbbaba4067b2ee1e399f9e22
Renzo Demarco <renzo11demarco@gmail.com>
2024-03-23 17:52:40 -0300

    new codewars challenge

--- detect-pangram.js
+++ detect-pangram.js
@@ -5,6 +5,7 @@
 // My Solution: 
 
 function isPangram(string){
-  const regex = /^(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z).*$/i;
-  return regex.test(string)
+  return 'abcdefghijklmnopqrstuvwxyz'
+    .split('')
+    .every(letter => string.toLowerCase().includes(letter));
 }
