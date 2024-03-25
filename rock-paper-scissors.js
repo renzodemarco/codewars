ce9e7171faefb5631e26804c3b2df340594635a3
Renzo Demarco <renzo11demarco@gmail.com>
2024-03-25 11:00:26 -0300

    new codewars challenge

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,12 +4,7 @@
 
 const rps = (p1, p2) => {
   if (p1 === p2) return 'Draw!'
-  const p1win = 'Player 1 won!'
-  const p2win = 'Player 2 won!'
-  if (p1 === 'rock' && p2 === 'scissors') return p1win
-  if (p1 === 'rock' && p2 === 'paper') return p2win
-  if (p1 === 'paper' && p2 === 'scissors') return p2win
-  if (p1 === 'paper' && p2 === 'rock') return p1win
-  if (p1 === 'scissors' && p2 === 'rock') return p2win
-  if (p1 === 'scissors' && p2 === 'paper') return p1win
+  const winRules = { paper: 'rock', scissors: 'paper', rock: 'scissors' }
+  if (p2 === winRules[p1]) return 'Player 1 won!'
+  else return 'Player 2 won!'
 };
