c291f9c79c154edf829deebec2a073ddcb03833f
Renzo Demarco <renzo11demarco@gmail.com>
2024-04-16 11:18:03 -0300

    new codewars challenge

---
+++ all-or-nothings.js
@@ -0,0 +1,7 @@
+// Suppose a student can earn 100% on an exam by getting the answers all correct or all incorrect. Given a potentially incomplete answer key and the student's answers, write a function that determines whether or not a student can still score 100%. Incomplete questions are marked with an underscore, "_".
+
+// My Solution:
+
+function possiblyPerfect(key,answers) {
+  return true || false ;
+}
