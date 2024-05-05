9098dc0fbce19aef36c2f1aa97e2a349776e7317
Renzo Demarco <renzo11demarco@gmail.com>
2024-05-05 20:30:52 -0300

    new codewars challenge

---
+++ remove-the-parentheses.js
@@ -0,0 +1,16 @@
+// Your task is to remove everything inside the parentheses as well as the parentheses themselves.
+
+// The example above would return:
+
+// My Solution:
+
+function removeParentheses(s){
+  let parentheses = 0
+  let res = []
+  for (i = 0; i < s.length; i++) {
+    if (s[i] === '(') parentheses++
+    if (!parentheses) res.push(s[i])
+    if (s[i] === ')') parentheses--
+  }
+  return res.join('')
+}
