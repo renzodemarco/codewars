41495e57e59570ff6005c31751591806856be58f
Renzo Demarco <renzo11demarco@gmail.com>
2024-05-19 22:29:09 -0300

    new codewars challenge

---
+++ find-the-missing-element-between-two-arrays.js
@@ -0,0 +1,10 @@
+// Given two integer arrays where the second array is a shuffled duplicate of the first array with one element missing, find the missing element.
+
+// Please note, there may be duplicates in the arrays, so checking if a numerical value exists in one and not the other is not a valid solution.
+
+// My Solution:
+
+function findMissing(arr1, arr2) {
+  // Your solution here
+  return 0
+}
