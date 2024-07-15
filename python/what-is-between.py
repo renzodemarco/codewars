ed8c04b780507eb51b7d2e6035fae50f4a5e4941
Renzo Demarco <renzo11demarco@gmail.com>
2024-07-15 19:46:59 -0300

    new codewars challenge

---
+++ python/what-is-between.py
@@ -0,0 +1,6 @@
+# Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+# My Solution:
+
+def between(a, b):
+    return list(range(a, b + 1))
