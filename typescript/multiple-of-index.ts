adcb495dd4375e3cf82a45e4444e44b6b993ac48
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-03 13:25:31 -0300

    new codewars challenge

--- typescript/multiple-of-index.ts
+++ typescript/multiple-of-index.ts
@@ -1,3 +1,11 @@
 // Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 
 // My Solution:
+
+export function multipleOfIndex(array: number[]): number[] {
+  const res: number[] = []
+  array.forEach((num, i) => {
+    if (num % i === 0 || num === 0) res.push(num)
+  })
+  return res
+}
