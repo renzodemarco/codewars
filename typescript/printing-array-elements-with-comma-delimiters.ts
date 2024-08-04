04bf378b98b71321eda0fd617963a50b6b747dfd
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-04 12:10:53 -0300

    new codewars challenge

---
+++ typescript/printing-array-elements-with-comma-delimiters.ts
@@ -0,0 +1,9 @@
+//Input: ["h","o","l","a"]
+
+// Output (String with comma delimited elements of the array in th same order): "h,o,l,a"
+
+// My Solution:
+
+export function printArray(array: number[]): string {
+  return array.join(',')
+}
