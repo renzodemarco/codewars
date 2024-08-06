792d0e4dac3ba4ff005b0a6d9bfa99538df3084f
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-06 13:24:57 -0300

    new codewars challenge

--- typescript/drink-about.ts
+++ typescript/drink-about.ts
@@ -13,3 +13,7 @@
 
 // My Solution:
 
+export function peopleWithAgeDrink(old: number): string {
+  const drink = (old < 14) ? 'toddy' : (old < 18) ? 'coke' : (old < 21) ? 'beer' : 'whisky'
+  return "drink " + drink
+}
