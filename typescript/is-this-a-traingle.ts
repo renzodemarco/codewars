a918314a83bd528db4567774c70ee86ab27beb8b
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-07 15:13:35 -0300

    new codewars challenge

---
+++ typescript/is-this-a-traingle.ts
@@ -0,0 +1,7 @@
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// My Solution:
+
+export function isTriangle(a: number, b: number, c: number): boolean {
+  return (a + b > c && b + c > a && c + a > b)
+}
