4507e6b17daa08efe120965e6228650b65e76ce4
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-10 13:55:20 -0300

    new codewars challenge

---
+++ typescript/no-zeros-for-heroes.ts
@@ -0,0 +1,16 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// My Solution:
+
+export function noBoringZeros(n: number): number {
+  if (n === 0) return 0
+  let str: string = n.toString()
+  while (str.endsWith('0')) {
+    str = str.slice(0, -1)
+  }
+  return parseInt(str)
+}
