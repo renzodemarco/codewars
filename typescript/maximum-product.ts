3d10acb79b9c8532622252de124b6777b8a74828
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-18 19:27:26 -0300

    new codewars challenge

--- typescript/maximum-product.ts
+++ typescript/maximum-product.ts
@@ -1,3 +1,11 @@
 // Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
 
 // My Solution:
+
+export function adjacentElementsProduct(arr: number[]): number {
+  const products: number[] = []
+  for (let i = 1; i < arr.length; i++) {
+    products.push(arr[i] * arr[i - 1])
+  }
+  return Math.max(...products)
+}
