d5b7ba07d6686f6bf4e0ff09a8c2bfb9fdc522ff
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-23 12:25:08 -0300

    new codewars challenge

---
+++ typescript/maximum-length-difference.ts
@@ -0,0 +1,7 @@
+// You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+// Find max(abs(length(x) − length(y)))
+
+// If a1 and/or a2 are empty return -1.
+
+// My Solution:
