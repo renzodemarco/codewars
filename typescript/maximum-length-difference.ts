465d5b9263e2b3ab9cacb8cb51b3c6cd4a7f21d6
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-23 13:10:31 -0300

    new codewars challenge

--- typescript/maximum-length-difference.ts
+++ typescript/maximum-length-difference.ts
@@ -5,3 +5,19 @@
 // If a1 and/or a2 are empty return -1.
 
 // My Solution:
+
+export const mxdiflg = (a1: string[], a2: string[]): number => {
+  if (a1.length === 0 || a2.length === 0) {
+    return -1;
+  }
+
+  const maxLenA1 = Math.max(...a1.map(str => str.length));
+  const minLenA1 = Math.min(...a1.map(str => str.length));
+  const maxLenA2 = Math.max(...a2.map(str => str.length));
+  const minLenA2 = Math.min(...a2.map(str => str.length));
+
+  const diff1 = Math.abs(maxLenA1 - minLenA2);
+  const diff2 = Math.abs(maxLenA2 - minLenA1);
+
+  return Math.max(diff1, diff2);
+};
