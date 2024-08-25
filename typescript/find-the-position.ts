ce34fe2961c06d4159007149e3d9c8e8d5dd78bd
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-25 00:38:03 -0300

    new codewars challenge

--- typescript/find-the-position.ts
+++ typescript/find-the-position.ts
@@ -5,3 +5,8 @@
 // Ouput: "Position of alphabet: 1"
 
 // My Solution:
+
+export function position(alphabet: string): string{
+  const position: number = alphabet.toLowerCase().charCodeAt(0) - 96
+  return "Position of alphabet: " + position;
+}
