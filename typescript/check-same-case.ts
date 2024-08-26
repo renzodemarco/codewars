ee1780ee892d3f472be1c01d4a44fc09e0171483
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-26 12:17:06 -0300

    new codewars challenge

--- typescript/check-same-case.ts
+++ typescript/check-same-case.ts
@@ -5,3 +5,14 @@
 // If both characters are letters, but not the same case, return 0
 
 // My Solution:
+
+export function sameCase(a: string, b: string): number {
+  if (!/[a-zA-Z]/.test(a) || !/[a-zA-Z]/.test(b)) {
+    return -1;
+  }
+
+  const isALower: boolean = a === a.toLowerCase();
+  const isBLower: boolean = b === b.toLowerCase();
+
+  return isALower === isBLower ? 1 : 0;
+}
