d03ac78442263d63bbea0c14bb4a34ce70508bf4
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-27 14:07:38 -0300

    new codewars challenge

--- typescript/sorted-yes-no-how.ts
+++ typescript/sorted-yes-no-how.ts
@@ -6,3 +6,12 @@
 // You can assume the array will always be valid, and there will always be one correct answer.
 
 // My Solution:
+
+export function isSortedAndHow(array: number[]): string {
+  const asc = [...array].sort((a, b) => a - b)
+  const desc = [...array].sort((a, b) => b - a)
+
+  if (array.every((n, i) => n === asc[i])) return "yes, ascending"
+  else if (array.every((n, i) => n === desc[i])) return "yes, descending"
+  return "no"
+}
