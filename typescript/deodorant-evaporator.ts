44cc82725fef8e01bb3574ba2e64684ce419f2a0
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-28 12:54:40 -0300

    new codewars challenge

--- typescript/deodorant-evaporator.ts
+++ typescript/deodorant-evaporator.ts
@@ -5,3 +5,13 @@
 // The program reports the nth day (as an integer) on which the evaporator will be out of use.
 
 // My Solution:
+
+export function evaporator(content: number, evapPerDay: number, threshold: number): number {
+  let days: number = 0
+  const limit: number = content * threshold / 100
+  do {
+    content -= content * (evapPerDay / 100)
+    days++
+  } while (content > limit)
+    return days
+}
