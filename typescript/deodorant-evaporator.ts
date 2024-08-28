cd9abe0a43a65df49bec70ddd990e213bb63bb85
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-28 12:41:32 -0300

    new codewars challenge

---
+++ typescript/deodorant-evaporator.ts
@@ -0,0 +1,7 @@
+// This program tests the life of an evaporator containing a gas.
+
+// We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+
+// The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+// My Solution:
