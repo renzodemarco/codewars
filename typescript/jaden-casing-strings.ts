90f8a7305ed5b78cf311b5cdf426e5a8da800e24
Renzo Demarco <renzo11demarco@gmail.com>
2024-09-08 00:34:05 -0300

    new codewars challenge

--- typescript/jaden-casing-strings.ts
+++ typescript/jaden-casing-strings.ts
@@ -3,3 +3,14 @@
 // Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
 
 // My Solution: 
+
+String.prototype.toJadenCase = function (): string {
+  return this
+    .split(' ')
+    .map((word: string) => word[0].toUpperCase() + word.slice(1))
+    .join(' ')
+}
+
+interface String {   
+  toJadenCase(): string;
+}
