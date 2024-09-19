041ced92db764dd67475c23c0f75a10ceb368a40
Renzo Demarco <renzo11demarco@gmail.com>
2024-09-19 13:14:27 -0300

    new codewars challenge

--- javascript/string-incrementer.js
+++ javascript/string-incrementer.js
@@ -4,3 +4,24 @@
 // If the string does not end with a number. the number 1 should be appended to the new string.
 
 // My Solution:
+
+function incrementString(strng) {
+  let numberStart = strng.length;
+  for (i = strng.length - 1; i >= 0; i--) {
+    if (!isNaN(strng[i])) {
+      numberStart = i;
+    } else {
+      break;
+    }
+  }
+  let letters = strng.slice(0, numberStart)
+  let number = strng.slice(numberStart);
+
+  if (numberStart === strng.length) {
+    return letters + "1";
+  }
+
+  let incrementedNumber = (parseInt(number, 10) + 1).toString().padStart(number.length, '0');
+
+  return letters + incrementedNumber;
+}
