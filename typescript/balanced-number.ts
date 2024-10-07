8471cf5ccd707bae1d70757751b26f595c622abc
Renzo Demarco <renzo11demarco@gmail.com>
2024-10-07 19:23:08 -0300

    new codewars challenge

--- typescript/balanced-number.ts
+++ typescript/balanced-number.ts
@@ -6,3 +6,19 @@
 
 // Given a number, find if it is balanced, and return the string "Balanced" or "Not Balanced" accordingly. The passed number will always be positive.
 
+export function balancedNum(number: number): string {
+  let array: number[] = number.toString().split('').map(Number)
+  let length: number = array.length
+
+  if (length <= 2) return "Balanced"
+
+  let middle: number = Math.floor(length / 2)
+
+  let left: number[] = array.slice(0, middle - (length % 2 === 0 ? 1 : 0))
+  let right: number[] = array.slice(middle + 1)
+
+  let leftSum: number = left.reduce((a, b) => a + b, 0)
+  let rightSum: number = right.reduce((a, b) => a + b, 0)
+
+  return leftSum === rightSum ? "Balanced" : "Not Balanced"
+}
