cc2694c9fc56ee073410d9a149d59d69c2703c6a
Renzo Demarco <renzo11demarco@gmail.com>
2024-10-07 13:48:47 -0300

    new codewars challenge

---
+++ typescript/balanced-number.ts
@@ -0,0 +1,8 @@
+// A balanced number is a number where the sum of digits to the left of the middle digit(s) and the sum of digits to the right of the middle digit(s) are equal.
+
+// If the number has an odd number of digits, then there is only one middle digit. (For example, 92645 has one middle digit, 6.) Otherwise, there are two middle digits. (For example, the middle digits of 1301 are 3 and 0.)
+
+// The middle digit(s) should not be considered when determining whether a number is balanced or not, e.g. 413023 is a balanced number because the left sum and right sum are both 5.
+
+// Given a number, find if it is balanced, and return the string "Balanced" or "Not Balanced" accordingly. The passed number will always be positive.
+
