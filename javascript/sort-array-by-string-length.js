a65560dd0705f64fd8cde17867b25f0a72ff2ca2
Renzo Demarco <renzo11demarco@gmail.com>
2024-10-12 14:36:13 -0300

    new codewars challenge

---
+++ javascript/sort-array-by-string-length.js
@@ -0,0 +1,7 @@
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// My Solution:
+
+function sortByLength (array) {
+  return array.sort((a, b) => a.length - b.length)
+}
