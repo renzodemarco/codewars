0d1e43368de154352be15ac3e0b4ac3ab4da1d37
Renzo Demarco <renzo11demarco@gmail.com>
2024-10-23 13:05:52 -0300

    new codewars challenge

---
+++ javascript/calculate-bmi.js
@@ -0,0 +1,16 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+// My Solution:
+
+function bmi(weight, height) {
+  const bmi = weight / (height ** 2)
+  return bmi <= 18.5 ? "Underweight" : bmi <= 25 ? "Normal": bmi <= 30 ? "Overweight" : "Obese"
+}
