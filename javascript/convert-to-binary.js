dadd3a7067ba506347bb39fb9f3f359b04659b72
Renzo Demarco <renzo11demarco@gmail.com>
2024-11-20 17:44:32 -0300

    new codewars challenge

---
+++ javascript/convert-to-binary.js
@@ -0,0 +1,7 @@
+// Given a non-negative integer b, write a function which returns an integer d such that the binary representation of b is the same as the decimal representation of d.
+
+// My Solution:
+
+function toBinary(n){
+  return Number(n.toString(2))
+}
