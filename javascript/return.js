1962829e239fcfff7cb85055abc93be738e18093
Renzo <renzo11demarco@gmail.com>
2024-11-30 14:14:36 -0300

    new codewars challenge

--- javascript/return.js
+++ javascript/return.js
@@ -10,3 +10,15 @@
 // subtraction = subt
 
 // My Solution:
+
+const add = (a, b) => a + b
+
+const divide = (a, b) => a / b
+
+const multiply = (a, b) => a * b
+
+const mod = (a, b) => a % b
+
+const exponent = (a, b) => a ** b
+
+const subt = (a, b) => a - b
