14627f119aefcbbc45eb60bb30a009443908cb22
Renzo <renzo11demarco@gmail.com>
2024-12-12 23:32:27 -0300

    new codewars challenge

---
+++ javascript/take-a-ten-minute-walk.js
@@ -0,0 +1,11 @@
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+function isValidWalk(walk) {
+  if (walk.length !== 10) return false
+  let vertical = 0
+  let horizontal = 0
+  for (i = 0; i < walk.length; i++) {
+    walk[i] === 'n' ? vertical++ : walk[i] === 's' ? vertical-- : walk[i] === 'e' ? horizontal++ : horizontal--
+  }
+  return vertical === 0 && horizontal === 0
+}
