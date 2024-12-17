2e5811578c2c7a27e138d6d8ed026261e3830187
Renzo <renzo11demarco@gmail.com>
2024-12-17 19:30:18 -0300

    new codewars challenge

---
+++ javascript/are-they-the-same.js
@@ -0,0 +1,24 @@
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11] 
+// b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
+// Remarks
+// a or b might be [] or {} (all languages except R, Shell).
+// a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+// If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+
+function comp(a, b) {
+  if (!Array.isArray(a) || !Array.isArray(b)) return false
+  a.sort((a, b) => a - b)
+  b.sort((a, b) => a - b)
+  for (i = 0; i < a.length; i++) {
+    if (Math.pow(a[i], 2) !== b[i]) return false
+  }
+  return true
+}
