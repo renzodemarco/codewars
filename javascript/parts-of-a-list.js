3bb35e4071b5a796b9a2fddd3d6abaf8fcf014d2
Renzo <renzo11demarco@gmail.com>
2025-01-03 23:04:05 -0300

    new codewars challenge

---
+++ javascript/parts-of-a-list.js
@@ -0,0 +1,17 @@
+// Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+// Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+// Each part will be in a string
+// Elements of a pair must be in the same order as in the original array.
+
+// My Solution:
+
+function partlist(arr) {
+  let res = []
+  for (i = 1; i < arr.length; i++) {
+    const first = arr.slice(0, i).join(' ')
+    const last = arr.slice(i).join(' ')
+    res.push([first, last])
+  }
+  return res
+}
