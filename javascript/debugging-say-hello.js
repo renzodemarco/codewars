dacdc612b0f87b7482c2c94f7e7e0642298e289b
Renzo <renzo11demarco@gmail.com>
2025-01-13 18:24:47 -0300

    new codewars challenge

---
+++ javascript/debugging-say-hello.js
@@ -0,0 +1,13 @@
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Original code:
+
+// function sayHello(name) {
+//   return 'Hello'
+// }
+
+// My Solution:
+
+function sayHello(name) {
+  return 'Hello, ' + name
+}
