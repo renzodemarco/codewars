8f258643faf1bec1e71111ef7238f56ead00b614
Renzo <renzo11demarco@gmail.com>
2025-01-14 22:37:17 -0300

    new codewars challenge

---
+++ javascript/regular-ball-super-ball.js
@@ -0,0 +1,7 @@
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+function Ball(ballType) {
+  // ! your code goes here
+}
