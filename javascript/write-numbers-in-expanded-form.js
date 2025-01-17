f751766f21f07103f171a136c7e1782971c8681a
Renzo <renzo11demarco@gmail.com>
2025-01-16 23:06:29 -0300

    new codewars challenge

---
+++ javascript/write-numbers-in-expanded-form.js
@@ -0,0 +1,11 @@
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+//    12 --> "10 + 2"
+//    45 --> "40 + 5"
+// 70304 --> "70000 + 300 + 4"
+
+// NOTE: All numbers will be whole numbers greater than 0.
+
+function expandedForm(num) {
+  // ! Your code here
+}
