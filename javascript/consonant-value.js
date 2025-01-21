f4ab3e05e63b816323fd3550d4e93b56ae37466b
Renzo <renzo11demarco@gmail.com>
2025-01-21 15:26:12 -0300

    new codewars challenge

---
+++ javascript/consonant-value.js
@@ -0,0 +1,26 @@
+// Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings. Consonants are any letters of the alphabet except "aeiou".
+
+// We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+
+// For example, for the word "zodiacs", let's cross out the vowels. We get: "z o d ia c"
+
+// "zodiac" -> 26
+// The consonant substrings are: "z", "d" and "c" with values "z" = 26, "d" = 4 and "c" = 3. The highest is 26.
+
+// "strength" -> 57
+// The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. The highest is 57.
+
+// My Solution:
+
+const obj = {
+  a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+}
+
+function solve(s) {
+  const string = s.replace(/[aeiou]/g, ' ');
+  const arr = string.split(' ')
+  const values = arr.map(sub => {
+    return sub.length === 0 ? 0 : sub.length === 1 ? obj[sub] : sub.split('').reduce((a, b) => a + obj[b], 0)
+  })
+  return Math.max(...values)
+}
