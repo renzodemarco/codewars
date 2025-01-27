50359ee5f161870deecadfd9697fa1ccb56d0778
Renzo <renzo11demarco@gmail.com>
2025-01-26 22:04:30 -0300

    new codewars challenge

---
+++ javascript/area-of-a-square.js
@@ -0,0 +1,7 @@
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+// My Solution:
+
+function squareArea(a) {
+  return Math.pow((2 * a) / Math.PI, 2)
+}
