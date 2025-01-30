bb763def520b7be756ce606067282ee56ab0d64c
Renzo <renzo11demarco@gmail.com>
2025-01-30 17:39:09 -0300

    new codewars challenge

---
+++ javascript/contamination.js
@@ -0,0 +1,13 @@
+// An AI has infected a text with a character!!
+
+// This text is now fully mutated to this character.
+
+// Starting with the original text, and given a character, return the text once it has been mutated so that all of the characters in the original text have been replaced with the character.
+
+// If the text or the character are empty, return an empty string.
+
+// My Solution:
+
+function contamination(text, char){
+  return char.repeat(text.length)
+}
