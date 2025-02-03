359d25ce4a30b4221276fcae905e934e92767ecd
Renzo <renzo11demarco@gmail.com>
2025-02-03 15:49:31 -0300

    new codewars challenge

---
+++ javascript/sum-the-strings.js
@@ -0,0 +1,7 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// My Solution:
+
+function sumStr(a, b) {
+  return String(Number(a) + Number(b))
+}
