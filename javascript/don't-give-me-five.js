64fedfcc2d3449b790d525f98cf320bfac730aa3
Renzo <renzo11demarco@gmail.com>
2025-02-18 22:04:08 -0300

    new codewars challenge

---
+++ javascript/don't-give-me-five.js
@@ -0,0 +1,11 @@
+// In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+// My Solution:
+
+function dontGiveMeFive(start, end) {
+  let count = []
+  for (i = start; i <= end; i++) {
+    if (!i.toString().split('').includes('5')) count.push(i)
+  }
+  return count.length
+}
