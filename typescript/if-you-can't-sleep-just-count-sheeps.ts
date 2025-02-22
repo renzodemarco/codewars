e2dc255400cb51905636a13c385a32ffc9e3282d
Renzo <renzo11demarco@gmail.com>
2025-02-22 11:26:08 -0300

    new codewars challenge

---
+++ typescript/if-you-can't-sleep-just-count-sheeps.ts
@@ -0,0 +1,11 @@
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers. 
+
+// My Solution:
+
+export function countSheep(n: number): string {
+  let res: string = ""
+  for (let i = 1; i <= n; i++) {
+    res += `${i} sheep...`
+  }
+  return res
+}
