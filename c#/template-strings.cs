5381b3d68435562623166e544ce9a7ba2b28603d
Renzo <renzo11demarco@gmail.com>
2025-03-06 18:18:53 -0300

    new codewars challenge

---
+++ c#/template-strings.cs
@@ -0,0 +1,14 @@
+// Your task is to return the correct string using the Template String Feature.
+
+// Input: Two Strings, no validation is needed.
+
+// Output: You must output a string containing the two strings with the word ```' are '```
+
+// My Solution:
+
+public class Templates{
+  public static string TempleStrings(string obj, string feature)
+  {
+      return $"{obj} are {feature}";
+  }
+}
