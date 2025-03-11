b3c497a8c5a3235992d0057d2aeefa5d602c6380
Renzo <renzo11demarco@gmail.com>
2025-03-11 14:08:09 -0300

    new codewars challenge

--- c#/lost-without-a-map.cs
+++ c#/lost-without-a-map.cs
@@ -2,3 +2,16 @@
 
 // My Solution:
 
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/csharp
+
+public class Kata
+{
+  public static int[] Maps(int[] x)
+  {
+    int[] res = new int[x.Length];
+    for (int i = 0; i < x.Length; i++) {
+      res[i] = x[i] * 2;
+    }
+    return res;
+  }
+}
